b7e3ca6563674abb63d960377ed5bb266633ed19
Elizaveta99 <30577965+Elizaveta99@users.noreply.github.com>
2018-03-26 18:46:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
 
 namespace Stack
 {
@@ -10,34 +12,34 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US", false);
             Console.WriteLine("Введите тип стека (на массивах - 1, на списках - 2) : ");
             int type = Int32.Parse(Console.ReadLine());         
-            IStack stack;
+            IStack<int> stack;
             if (type == 1)
             {
                 Console.WriteLine("Введите размер стека : ");
                 int number = Int32.Parse(Console.ReadLine());
-                stack = new Stack_Array(number);
+                stack = new Stack_Array<int>(number);
             }
             else
-                stack = new Stack_List();
+                stack = new Stack_List<int>();
 
             try
             {
                 Console.WriteLine("Введите элемент стека или N, если хотите завершить ввод : ");
                 while (true)
                 {
-                    char c = Char.Parse(Console.ReadLine());
-                    if (c != 'N')
+                    string temps = Console.ReadLine();
+                    if (temps[0] != 'N')
                     {
-                        string temps = Convert.ToString(c);
                         int temp = Int32.Parse(temps);
                         stack.Push(temp);                     
                     }
                     else break;
                 }
 
-                Console.WriteLine("Первый элемент стека : {0}", stack.Top());
+                Console.WriteLine("Первый элемент стека : {0}", stack.Top().ToString());
 
                 if (!stack.Pop())
                 {
@@ -47,7 +49,7 @@ namespace Stack
 
                 Console.WriteLine("Размер cтека (после удаления): {0}", stack.GetSize());
 
-                Console.WriteLine("Первый элемент стека после удаления : {0}", stack.Top());
+                Console.WriteLine("Первый элемент стека после удаления : {0}", stack.Top().ToString());
 
                 if (stack.isEmpty()) Console.WriteLine("Стек пуст.");
             }
